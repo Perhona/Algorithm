3a8d6dbe9735dc8b99b7a0a89ccb66390c8d870b
KIM NAM EUN <95855455+Perhona@users.noreply.github.com>
2022-06-27 22:32:59 +0900

    Update 알람 시계.java

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -6,26 +6,22 @@ public class Main {
     Scanner sc = new Scanner(System.in);
     int a = sc.nextInt();
     int b = sc.nextInt();
+
     if (b>45){
-      System.out.print(a);
-      System.out.println();
-      System.out.print(b-45);
+      System.out.println(a);
+      System.out.println(b-45);
     }
     if (b==45){
-      System.out.print(a);
-      System.out.println();
-      System.out.print("0");
+      System.out.println(a);
+      System.out.println("0");
     }
     if (b<45){
       if (a==0){
-        System.out.print("23");
-        System.out.println();
-        System.out.print(60+(b-45));
+        System.out.println("23");
       } else {
-        System.out.print(a-1);
-        System.out.println();
-        System.out.print(60+(b-45));
+        System.out.println(a-1);
       }
+      System.out.println(60+(b-45));
     }
   }
 }
