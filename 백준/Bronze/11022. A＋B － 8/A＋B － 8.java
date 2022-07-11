0399adc142d1e9ce1e0fda7b6fc6b5a1c9fb69f8
KIM NAM EUN <95855455+Perhona@users.noreply.github.com>
2022-07-11 10:25:09 +0900

    [Bronze III] Title: A+B - 8, Time: 184 ms, Memory: 16424 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int x = Integer.parseInt(br.readLine());
+    for (int i=1;i<x+1;i++){
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int a = Integer.parseInt(st.nextToken());
+      int b = Integer.parseInt(st.nextToken());
+      int result = a+b;
+      bw.write("Case #" + i + ": " + a + " + "+ b + " = " + result + "\n");
+    }
+    bw.flush();
+    bw.close();
+    br.close();
+  }
+}
