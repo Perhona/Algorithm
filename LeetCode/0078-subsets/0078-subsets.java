2495c29fb6269dde1f14835761fbe6f12c13d528
KIM NAM EUN <95855455+Perhona@users.noreply.github.com>
2024-08-08 18:28:33 +0900

    Time: 1 ms (53.21%), Space: 42.7 MB (28.59%) - LeetHub

--- LeetCode/0078-subsets/0078-subsets.java
+++ LeetCode/0078-subsets/0078-subsets.java
@@ -1,14 +1,18 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-                dfs(result, nums, 0, new ArrayDeque<>());
+        dfs(result, nums, 0, new ArrayDeque<>());
 
         return result;
     }
 
     private void dfs(List<List<Integer>> results, int[] nums, int index, Deque<Integer> path) {
+        // 모든 탐색 경로를 결과에 저장한다. 빈 집합('[]')도 맨 처음 저장된다.
         results.add(new ArrayList<>(path));
 
+        // 가장 처음 0이 index로 들어온다.
+        // 저장된 nums[0]은 다음 재귀함수 호출 시 먼저 저장된다.
+        // index는 1이 되어 nums[1]부터 반복을 시작한다.
         for (int i = index; i < nums.length; i++) {
             path.add(nums[i]);
             dfs(results, nums, i + 1, path);
