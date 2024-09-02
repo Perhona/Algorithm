f855630bd0ea746873e6cdad5bcee75ff2ce8448
KIM NAM EUN <perhona@kakao.com>
2024-09-02 20:25:28 +0900

    Time: 2 ms (69.27%), Space: 44.4 MB (35.05%) - LeetHub

--- LeetCode/0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ LeetCode/0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -26,20 +26,20 @@ class Solution {
     private TreeNode dfs(int[] preorder, int preStart, Map<Integer, Integer> inorderMap, int inStart, int inEnd) {
         // 1. 현재 트리의 루트 노드 찾기
         // 2. 왼쪽 노드 찾기 - 전위순회에서는 NLR 이므로 preStart는 +1씩, 중위 순회에서는 종료 idx가 순차적으로 좁혀진다.
-        // 3. 오른쪽 노드 찾기 - NLR이므로 preStart는 발견한 중위순회 idx(좌측노드 갯수) + 1부터 탐색, 중위순회에서는 시작
-        // idx에서 좁혀간다.
+        // 3. 오른쪽 노드 찾기 - NLR이므로 preStart는 발견한 중위순회 idx(좌측노드 갯수) + 1부터 탐색, 중위순회에서는 시작 idx에서 좁혀간다.
         if (preStart > preorder.length - 1 || inStart > inEnd) {
             return null;
         }
 
-        int rootVal = preorder[preStart]; // 현재 트리의 루트 값
+        int rootVal = preorder[preStart];           // 현재 트리의 루트 값
         TreeNode root = new TreeNode(rootVal);
-        int rootInIdx = inorderMap.get(rootVal); // 중위 순회에서 좌측 노드의 갯수이자 현재 root 노드의 인덱스
+        int rootInIdx = inorderMap.get(rootVal);    // 중위 순회에서 좌측 노드의 갯수이자 현재 root 노드의 인덱스
+        int leftSize = rootInIdx - inStart;         // 중위순회 배열에서 현재 root 노드의 인덱스 - 현재 중위순위 배열 시작값
 
-        root.left = dfs(preorder, preStart + 1, inorderMap, inStart, rootInIdx - 1); 
+        root.left = dfs(preorder, preStart + 1, inorderMap, inStart, rootInIdx - 1);
         // 전위 순회는 +1부터, 중위 순회는 end값 제한
-        root.right = dfs(preorder, preStart + rootInIdx - inStart + 1, inorderMap, rootInIdx + 1, inEnd); 
-        // 전위 순회는 현재 범위 중위 순회에서 발견한 좌측 노드 갯수 + 1부터, 중위순회는 start값 제한
+        root.right = dfs(preorder, preStart + leftSize + 1, inorderMap, rootInIdx + 1, inEnd);
+        // 전위 순회는 좌측 노드 갯수 + 1부터, 중위순회는 start값 제한
 
         return root;
     }
