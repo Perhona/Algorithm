1a0502fdcbba95bfe141eec10c568c9d3c4e8b71
KIM NAM EUN <perhona@kakao.com>
2024-09-12 18:05:48 +0900

    Time: 10 ms (26.15%), Space: 47.6 MB (5.92%) - LeetHub

---
+++ LeetCode/0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, Comparator.comparingInt(arr -> arr[0]));
+        List<int[]> resultList = new ArrayList<>();
+        dfs(resultList, intervals, intervals[0], 1 );
+
+        return listToArray(resultList);
+    }
+
+    private void dfs(List<int[]> result, int[][] arr, int[] a, int idx) {
+        // 비교하면서 연결고리가 없는 경우 result에 넣고, 있는 경우 dfs 계속
+        if (idx > arr.length-1) {
+            result.add(a);
+            return;
+        }
+
+        int x = a[0], y = a[1];
+        int x2 = arr[idx][0], y2 = arr[idx][1];
+        if (x <= x2 && x2 <= y) {
+            a[0] = x;
+            a[1] = Math.max(y, y2);
+        } else {
+            result.add(new int[]{x, y});
+            a[0] = x2;
+            a[1] = y2;
+        }
+
+        dfs(result, arr, a, idx + 1);
+    }
+
+    public static int[][] listToArray(List<int[]> list) {
+        int[][] array = new int[list.size()][];
+
+        for (int i = 0; i < list.size(); i++) {
+            array[i] = list.get(i);
+        }
+
+        return array;
+    }
+}
