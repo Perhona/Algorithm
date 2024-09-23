0db99b1c202568341eb3c7028504d13404ff33d9
KIM NAM EUN <perhona@kakao.com>
2024-09-23 18:36:06 +0900

    Time: 18 ms (6.84%), Space: 44.9 MB (5.06%) - LeetHub

---
+++ LeetCode/0179-largest-number/0179-largest-number.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String largestNumber(int[] nums) {
+        // 두번째 수부터 앞의 수와 비교하며 가장 큰 수를 앞으로 보낸다.
+
+        for (int i = 1; i < nums.length; i++) {
+            int b = nums[i];
+            int idx = i;
+            while (idx - 1 >= 0) {
+                int a = nums[idx - 1];
+                if (toSwap(a, b)) {
+                    int temp = a;
+                    nums[idx - 1] = b;
+                    nums[idx] = temp;
+                }
+                idx--;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int n : nums) {
+            sb.append(n);
+        }
+        String answer = sb.toString();
+        if (answer.replaceAll("0", "").isBlank()){
+            return "0";
+        }
+        return answer;
+        }
+    private boolean toSwap(int a, int b) {
+        String aStr = String.valueOf(a);
+        String bStr = String.valueOf(b);
+        return (aStr + bStr).compareTo(bStr + aStr) < 0;
+    }
+}
