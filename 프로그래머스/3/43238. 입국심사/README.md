0f266663d2410f2dcfb49e73008b4d5e8884cbe6
KIM NAM EUN <perhona@kakao.com>
2024-10-06 11:28:38 +0900

    [level 3] Title: 입국심사, Time: 52.91 ms, Memory: 92.8 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238) 
 
 ### 성능 요약
 
-메모리: 100 MB, 시간: 103.75 ms
+메모리: 92.8 MB, 시간: 52.91 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 05일 12:06:19
+2024년 10월 06일 11:28:38
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -2,26 +2,25 @@ import java.util.Arrays;
 
 class Solution {
     public long solution(int n, int[] times) {
-        // 시간 배열을 오름차순 정렬
-        Arrays.sort(times);
+       long left = 1;
+        long right = (long) times[times.length - 1] * n;
+//        long mid = left + ((right - left) / 2);
+//        boolean isMultiple = isMultiple(mid, n, times);
 
-        long left = 1;  // 최소 시간 (1분)
-        long right = (long) times[times.length - 1] * n;  // 최대 시간 (가장 오래 걸리는 심사관이 모든 사람을 처리할 때)
-
-        // 이진 탐색 시작
-        while (left < right) {
-            long mid = left + (right - left) / 2;  // 중간 값 계산
-            long countN = findN(mid, times);
-
-            if (countN < n) {
-                left = mid + 1;  // mid 시간 내에 모든 사람을 처리할 수 없다면, 더 큰 시간이 필요
-            } else {
-                right = mid;  // mid 시간 내에 모든 사람을 처리할 수 있다면, 더 작은 시간이 가능할 수도 있음
+        while (left <= right) {
+            long mid = left + ((right - left) / 2);
+            long findN = findN(mid, times);
+//            isMultiple = isMultiple(mid, n, times);
+            if (findN < n) {
+                left = mid + 1;
+            } else if (n <= findN
+//                    || !isMultiple
+            ) {
+                right = mid - 1;
             }
         }
-        return left;  // 최소 시간을 반환
+        return left;
     }
-
     public long findN(long mid, int[] times) {
         long result = 0;
         for (long i : times) {
