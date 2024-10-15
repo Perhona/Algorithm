c690e149d827ef8d789c76539a95864bc28e2d95
KIM NAM EUN <perhona@kakao.com>
2024-10-16 03:12:03 +0900

    Time: 29 ms (5.24%), Space: 59.9 MB (5.33%) - LeetHub

--- LeetCode/0134-gas-station/0134-gas-station.java
+++ LeetCode/0134-gas-station/0134-gas-station.java
@@ -1,29 +1,35 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
         List<int[]> existGasList = new ArrayList<>();
+        int sum = 0;
         for (int i = 0; i < gas.length; i++) {
-            existGasList.add(new int[]{i, gas[i] - cost[i]});
+            int diff = gas[i] - cost[i];
+            existGasList.add(new int[]{i, diff});
+            sum += diff;
         }
+        if (sum < 0) {
+            return -1;
+        }
+
         Queue<int[]> gasQueue = new ArrayDeque<>(existGasList);
+        List<int[]> usedGasList = new ArrayList<>();
         int[] result = new int[]{-1, 0};
-        int count = existGasList.size();
 
         while (!gasQueue.isEmpty()) {
-            if (count <= 0 && result[1] == 0) {
-                return -1;
-            }
-
             int[] current = gasQueue.poll();
-            count--;
+            usedGasList.add(current);
 
             if (result[0] == -1) {
                 result[0] = current[0];
             }
             result[1] += current[1];
-            if (result[1] <= 0 && !gasQueue.isEmpty()) {
+
+            // 잔여 가스가 0 이하이면 사용했던 가스 다시 큐에 add 후 재시쟉
+            if (result[1] <= 0 && usedGasList.size() != existGasList.size()) {
                 result[0] = -1;
                 result[1] = 0;
-                gasQueue.add(current);
+                gasQueue.addAll(usedGasList);
+                usedGasList.clear();
             }
         }
         return result[0];
