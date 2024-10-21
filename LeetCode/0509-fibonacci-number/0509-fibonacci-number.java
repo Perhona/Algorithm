9ed84cdb0ff5f1b8fdb07b9239ad84085927d071
KIM NAM EUN <perhona@kakao.com>
2024-10-21 18:30:57 +0900

    Time: 0 ms (100%), Space: 40.2 MB (51.57%) - LeetHub

--- LeetCode/0509-fibonacci-number/0509-fibonacci-number.java
+++ LeetCode/0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,11 +1,14 @@
 class Solution {
+    private int[] nums = new int[31];
+
     public int fib(int n) {
-        if (n == 0) {
-            return 0;
+        if (n <= 1) {
+            return n;
         }
-        if (n == 1) {
-            return 1;
+
+        if (nums[n] == 0) {
+            nums[n] = fib(n - 1) + fib(n - 2);
         }
-        return fib(n - 2) + fib(n - 1);
+        return nums[n];
     }
 }
