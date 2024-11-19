a74c4233731fc5f3fdd44e6a24b83e7a45ab21bb
KIM NAM EUN <perhona@kakao.com>
2024-11-19 17:04:13 +0900

    [level 1] Title: 최소직사각형, Time: 1.88 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,22 +1,12 @@
 class Solution {
     public int solution(int[][] sizes) {
-        for (int i=0;i<sizes.length;i++) {
-            int curX = sizes[i][0];
-            int curY = sizes[i][1];
-            if (curX > curY) {
-                sizes[i][0] = curY;
-                sizes[i][1] = curX;
-            }
-        }
-        
         int minX = 0, minY = 0;
         for (int i=0;i<sizes.length;i++) {
             int curX = sizes[i][0];
             int curY = sizes[i][1];
-            minX = Math.max(curX, minX);
-            minY = Math.max(curY, minY);
+            minX = Math.max(minX, Math.min(curX, curY));
+            minY = Math.max(minY, Math.max(curX, curY));
         }
-        
         return minX * minY;
     }
 }
