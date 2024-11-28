90ed634cbbd2edaf4d9c4bacdbfba2bee686ca4f
KIM NAM EUN <perhona@kakao.com>
2024-11-29 00:00:11 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 1.79 ms, Memory: 58.3 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -1,73 +1,35 @@
+import java.util.Arrays;
+
 class Solution {
-    private static final String start = "00:00";
-    private static String current = "";
+    private int convertToSec(String time) { // 초로 변경
+        String[] splitedTime = time.split(":");
+        return Integer.parseInt(splitedTime[0]) * 60 + Integer.parseInt(splitedTime[1]);
+    }
+    
+    private boolean isInSkipRange(int time, int opStartSec, int opEndSec) { // 스킵 여부 확인
+        return opStartSec <= time && time < opEndSec;
+    }
+    
     public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
-        current = pos;
-        System.out.println("current: "+current);
-        skipOp(op_start, op_end);
+        int videoLenSec = convertToSec(video_len),
+        posSec = convertToSec(pos),
+        opStartSec = convertToSec(op_start),
+        opEndSec = convertToSec(op_end);
+        
+        posSec = isInSkipRange(posSec, opStartSec, opEndSec) ? opEndSec : posSec;
         
         for (String command : commands) {
-            System.out.println("comand: " +command);
-            if (command.equals("prev")) {
-                current = calculate(current, -10);
-                if (!isBigger(start, current)) {
-                    current = start;
-                }
-            }
             if (command.equals("next")) {
-                current = calculate(current, 10);
-                if (isBigger(video_len, current)) {
-                    current = video_len;
-                }
+                posSec = Math.min(posSec + 10, videoLenSec);
+            } else {
+                posSec = Math.max(posSec - 10, 0);
             }
-            System.out.println("check prev or next: " + current);
-            skipOp(op_start, op_end);
-            System.out.println("check op: " + current);
-        }
-        return current;
-    }
-    
-    private void skipOp(String op_start, String op_end) {
-        if (isBigger(op_start, current) && !isBigger(op_end, current)) {
-            System.out.println("move to op_end");
-            current = op_end;
-        }
-    }
-    private boolean isBigger(String baseTime, String newTime) {
-        String[] baseTimeArr = baseTime.split(":");
-        String[] newTimeArr = newTime.split(":");
-        int baseMinute = Integer.parseInt(baseTimeArr[0]);
-        int baseSecond = Integer.parseInt(baseTimeArr[1]);
-        int newMinute = Integer.parseInt(newTimeArr[0]);
-        int newSecond = Integer.parseInt(newTimeArr[1]);
-        
-        if (baseMinute - newMinute > 0) {
-            return false;
-        } else if (baseMinute - newMinute < 0) {
-            return true;
+            posSec = isInSkipRange(posSec, opStartSec, opEndSec) ? opEndSec : posSec;
         }
         
-        if (baseSecond - newSecond > 0) {
-            return false;
-        } else {
-            return true;
-        }
-    }
-    
-    private String calculate (String baseTime, int number) {
-        String[] baseTimeArr = baseTime.split(":");
-        int baseMinute = Integer.parseInt(baseTimeArr[0]);
-        int baseSecond = Integer.parseInt(baseTimeArr[1]);
-        baseSecond += number;
-        System.out.printf("baseTime: %s, number: %d, baseSecond: %d \n", baseTime, number, baseSecond);
-        if (baseSecond < 0) {
-            baseMinute--;
-            baseSecond += 60;
-        }
-        if (baseSecond > 60) {
-            baseMinute++;
-            baseSecond -= 60;
-        }
-        return String.format("%02d:%02d", baseMinute, baseSecond);
+        // 분, 초 변환
+        int minute = posSec / 60;
+        int second = posSec % 60;
+        return String.format("%02d:%02d", minute, second);
     }
 }
