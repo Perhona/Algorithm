e31b667659f06536a64b1da499a690086b982f4d
KIM NAM EUN <perhona@kakao.com>
2024-12-03 00:22:47 +0900

    [level 2] Title: 전화번호 목록, Time: 276.22 ms, Memory: 246 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,13 +1,20 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
         // 1. for문 두 번 사용 시 시간 초과
-        Arrays.sort(phone_book);
+        // 2. 배열 정렬 후 다음 요소와의 비교(OK
+        // 3. 해시셋 사용
         
-        for (int i=0;i<phone_book.length -1;i++) {
-            String phoneNumber = phone_book[i];
-            if (phone_book[i+1].startsWith(phoneNumber)) return false;
+        Set<String> phoneSet = new HashSet<>();
+        for (String number : phone_book) {
+            phoneSet.add(number);
+        }
+        
+        for (String number : phone_book) {
+            for (int i=1;i<number.length();i++) {
+                if (phoneSet.contains(number.substring(0,i))) return false;
+            }
         }
         
         return true;
