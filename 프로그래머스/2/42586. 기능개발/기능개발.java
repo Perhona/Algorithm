4051e24f05fcd06fbabb4afa57def400a502f33e
KIM NAM EUN <perhona@kakao.com>
2024-12-04 00:48:07 +0900

    [level 2] Title: 기능개발, Time: 3.64 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,24 +2,24 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int[] days = new int[progresses.length];
+        // queue를 이용해 배포 그룹 만들어주기
+        Queue<Integer> queue = new LinkedList<>();
+        List<Integer> answer = new ArrayList<>();
+        
         for (int i=0;i<progresses.length;i++) {
             int todo = (100 - progresses[i]);
-            days[i] = (int) Math.ceil((double) todo / speeds[i]);
-        }
-
-        Deque<Integer> answer = new ArrayDeque<>();
-        answer.offer(1);
-        int lastTodo = days[0];
-        
-        for (int i=1;i<days.length;i++) {
-            if (days[i] <= lastTodo) {
-                answer.offer(answer.pollLast() + 1);
-            } else {
-                lastTodo = days[i];
-                answer.offer(1);
+            int remainDay = (int) Math.ceil((double) todo / speeds[i]);
+            
+            // 이전 일수보다 남은 일수가 많으면 배포 그룹 클리어
+            if (!queue.isEmpty() && queue.peek() < remainDay) {
+                answer.add(queue.size());
+                queue.clear();
             }
+            queue.offer(remainDay);
         }
+        
+        answer.add(queue.size()); // 마지막 배포 그룹 클리어
+
         return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
