e3f38d87716f376a151de5d3764347ed9144f645
KIM NAM EUN <perhona@kakao.com>
2025-05-02 11:41:02 +0900

    [Gold V] Title: 두 용액, Time: 348 ms, Memory: 29956 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -9,22 +9,28 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         int[] result = new int[2];
         int[] liquids = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).sorted().toArray();
+
         int leftIdx = 0;
         int rightIdx = liquids.length - 1;
         int min = Integer.MAX_VALUE;
 
         while (leftIdx < rightIdx) {
             int sum = liquids[leftIdx] + liquids[rightIdx];
-            if (Math.abs(sum) < min) {
-                min = Math.abs(sum);
+            int absSum = Math.abs(sum);
+
+            /* 절댓값이 작아졌는지 확인 -> 정답을 갱신할지 기준 */
+            /* 틀렸던 이유: 절댓값이 작아졌다는 사실은 "정답을 갱신할 조건"일 뿐, "어떤 포인터를 움직일지 결정하는 기준"은 아님 */
+            if (absSum < min) {
+                min = absSum;
                 result[0] = liquids[leftIdx];
                 result[1] = liquids[rightIdx];
             }
 
-            if (sum >= 0) {
-                rightIdx -= 1;
+            /* 포인터를 움직이는 기준 -> 기준값 (0) 이상/이하/같음 */
+            if (sum < 0) {
+                leftIdx++;
             } else {
-                leftIdx += 1;
+                rightIdx--;
             }
         }
         System.out.println(result[0] + " " + result[1]);
