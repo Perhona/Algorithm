4ef59ba61de3a43f2c25f4845838f55e0b50717f
KIM NAM EUN <perhona@kakao.com>
2025-05-06 09:46:13 +0900

    [Bronze II] Title: Hashing, Time: 108 ms, Memory: 14364 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,19 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int L = Integer.parseInt(br.readLine());
         char[] input = br.readLine().toCharArray();
-        long sum = 0;
-        int r = 31;
+        BigInteger sum = BigInteger.ZERO;
+        BigInteger r = BigInteger.valueOf(31);
+        BigInteger divideNum = BigInteger.valueOf(1_234_567_891);
 
         for (int i = 0; i < input.length; i++) {
-            int toInt = input[i] - 'a' + 1;
-            sum += (long) (toInt * (Math.pow(r, i)));
+            BigInteger toInt = BigInteger.valueOf(input[i] - 'a' + 1);
+            BigInteger pow = r.pow(i);
+            sum = sum.add(pow.multiply(toInt));
         }
-        System.out.println(sum % 1_234_567_891);
+        System.out.println(sum.mod(divideNum));
     }
 }
